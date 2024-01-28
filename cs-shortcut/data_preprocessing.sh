96b8333ecd2f8fc9e2b0084def71fcc6ebafdbbf
Fanghua Ye <smartye_uestc@hotmail.com>
2024-01-28 18:10:25 +0800

    Update data_preprocessing.sh

--- cs-shortcut/data_preprocessing.sh
+++ cs-shortcut/data_preprocessing.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 TASK=qrecc
-DATA_PATH=/home/ye/CQR/datasets
-OUTPUT_PATH=/home/ye/CQR/datasets/preprocessed
+DATA_PATH=/home/user/InfoCQR/datasets
+OUTPUT_PATH=/home/user/InfoCQR/datasets/preprocessed
 
 python data_preprocessing.py \
   --task ${TASK} \
   --data_path ${DATA_PATH} \
   --output_path ${OUTPUT_PATH} \
   --max_passage_length 384 \
-  --pyserini_index_path ${DATA_PATH}/${TASK}/pyserini_index \
   --test_collection_path ${DATA_PATH}/${TASK}
   
